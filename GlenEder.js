8f797469a0468e36ebcdbe0b651d8050ac050cef
gleneder <geder@purdue.edu>
2021-01-01 13:40:23 -0500

    Resets site to blank canvas

--- GlenEder.js
+++ GlenEder.js
@@ -1,68 +1,110 @@
-let isMenuOpen = 0;
 
+let terminal = null
 
-function changeMenuState(x) {
-	x.classList.toggle("change");
+window.addEventListener('load', () => {
 
-	if(isMenuOpen == 1) {
-		document.getElementById("menu").style.width = "0%";
-		isMenuOpen = 0;
-	}else {
-		//console.log(window.innerWidth);
-		if(window.innerWidth > 1650) {
-			document.getElementById("menu").style.width = "8%";
-		}else {
-			document.getElementById("menu").style.width = "13%";
+	//set terminal
+	terminal = document.getElementById("termOutput")
+	terminal.value = ""
+
+	//Loading terminal animation
+	loadTerminal()
+
+	//Add listener for ENTER
+	let input = document.getElementById("termInput")
+	input.value = ""
+	input.addEventListener("keydown", (e) => {
+			if(e.key === "Enter") {
+				handleCommand(input.value)
+				input.value = ""
+			}
+	})
+
+	//set focus to terminput on screen click
+	window.addEventListener('click', () => {
+		input.focus()
+	})
+
+})
+
+function loadTerminal() {
+
+	terminal.value = "Loading"
+	let percent = 0;
+	let x = setInterval(() => {
+		percent += Math.floor(Math.random() * 20 + 5)
+		terminal.value += '.'
+
+		if(percent >= 100) {
+			clearInterval(x)
+			terminal.value = ""
+			let message = "Glen Michael Austin Eder\n\t" +
+				"Aspiring Software Developer\n\t" +
+				"Experienced Leader\n\n" +
+				"Enter command to proceed, use 'help' to see command list\n"
+			printToTerm(message)
 		}
-		isMenuOpen = 1;
+	}, 200)
+
+
+
+}
+
+function handleCommand(command) {
+
+	//access output text area
+	let out = document.getElementById("termOutput")
+
+	//check command list
+	switch (command) {
+
+		case "about":
+			about()
+			break
+		case "help":
+			help()
+			break
+		case "resume":
+			printToTerm("Loading resume...\n")
+			showResume()
+			break;
+		case "clear":
+			terminal.value = ""
+			break
+
+		//throw error statement
+		default:
+			let message = "'" + command + "' is not a command\n\tUse 'help' to see all commands\n"
+			printToTerm(message)
 	}
 
-	
 }
 
-function showPDF() {
-		document.getElementById("resumeImageContainer").style.width = "100%";
-		document.getElementById("mainMenuIcon").style.visibility = "hidden";
+function showResume() {
+	document.getElementById("resumeContainer").style.width= "100%"
 }
 
-function closePDF() {
-		document.getElementById("resumeImageContainer").style.width = "0%";
-		document.getElementById("mainMenuIcon").style.visibility = "visible";
+function help() {
+
+	let message = "Command List\n" +
+		"\tabout -- render general information about Glen Eder\n" +
+		"\tresume -- view Glen Eder's Resume\n" +
+		"\tcontact -- show Glen Eder's contact information\n" +
+		"\tsocials --  display social media links\n" +
+		"\tclear -- clear the terminal\n"
+
+	printToTerm(message)
 }
 
-
-function showContactInfo() {
-		document.getElementById("contactContainer").style.width = "100%";
-		document.getElementById("mainMenuIcon").style.visibility = "hidden";
+function about() {
+	document.getElementById("termOutput").value += "Redirecting to about...\n"
 }
 
-function closeContactInfo() {
-		document.getElementById("contactContainer").style.width = "0%";
-		document.getElementById("mainMenuIcon").style.visibility = "visible";
-}
-
-function showAboutInfo() {
-	document.getElementById("aboutInfoContainer").style.visibility = "visible"
-	document.getElementById("aboutContainer").style.width = "100%";
-	document.getElementById("aboutContainer").style.overflowY = "scroll";
-	document.getElementById("mainMenuIcon").style.visibility = "hidden";
-	
-}
-
-function closeAboutInfo() {
-	document.getElementById("aboutInfoContainer").style.visibility = "hidden"
-	document.getElementById("aboutContainer").style.width = "0%";
-	document.getElementById("aboutContainer").style.overflowY = "hidden";
-	document.getElementById("mainMenuIcon").style.visibility = "visible";
-	
-}
-
-function showGameTab() {
-	document.getElementById("gameTab").style.width = "100%";
-	document.getElementById("mainMenuIcon").style.visibility = "hidden";
-}
-
-function closeGameTab() {
-	document.getElementById("gameTab").style.width = "0%";
-	document.getElementById("mainMenuIcon").style.visibility = "visible";
+function printToTerm(output) {
+	if(output) {
+		setTimeout(() => {
+			terminal.value += output.charAt(0);
+			printToTerm(output.substring(1))
+		}, 5)
+	}
 }
