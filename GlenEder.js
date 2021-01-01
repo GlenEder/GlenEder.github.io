13d359ff3542f102b3f968bca138e60b27955db5
gleneder <geder@purdue.edu>
2021-01-01 16:14:09 -0500

    Adds photos below without paralax cause yeah

--- GlenEder.js
+++ GlenEder.js
@@ -1,9 +1,4 @@
 
 window.addEventListener('load', () => {
 
-	let position = screen.height + "px"
-	console.log("Pos: " + position)
-
-	let photoBlock = document.getElementById("photography")
-	photoBlock.style.top = position
 })
